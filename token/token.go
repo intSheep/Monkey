1e27f92dcee48d998c57883efb76b963bcb1b52c
intSheep <1098204193@qq.com>
2024-03-08 22:18:29 +0800

    fix token.go

--- token/token.go
+++ token/token.go
@@ -1,22 +1,33 @@
 package token
 
-import "go/token"
+type TokenType string
 
-// token/token.go
+type Token struct {
+	Type    TokenType
+	Literal string
+}
 
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
 
-	// 标识符+字面量
-	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT   = "INT"   // 1343456
+	IDENT = "IDENT"
+	INT   = "INT"
 
-	// 运算符
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	ASTERISK = "*"
+	SLASH    = "/"
+	BANG     = "!"
 
-	// 分隔符
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
+	LT_EQ  = "<="
+	GT_EQ  = ">="
+
 	COMMA     = ","
 	SEMICOLON = ";"
 
@@ -24,41 +35,15 @@ const (
 	RPAREN = ")"
 	LBRACE = "{"
 	RBRACE = "}"
+	LBRACK = "["
+	RBRACK = "]"
 
-	// 关键字
-	FUNCTION = "FUNCTION"
-	LET      = "LET"
+	FUNCTION = "fun"
+	LET      = "let"
+	IF       = "if"
+	ELSE     = "else"
+	ELSE_IF  = "elif"
+	TRUE     = "true"
+	FALSE    = "false"
+	RETURN   = "return"
 )
-
-func (l *Lexer) NextToken() token.Token {
-	var tok token.Token
-
-	switch l.ch {
-	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
-	case ';':
-		tok = newToken(token.SEMICOLON, l.ch)
-	case '(':
-		tok = newToken(token.LPAREN, l.ch)
-	case ')':
-		tok = newToken(token.RPAREN, l.ch)
-	case ',':
-		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
-	case '{':
-		tok = newToken(token.LBRACE, l.ch)
-	case '}':
-		tok = newToken(token.RBRACE, l.ch)
-	case 0:
-		tok.Literal = ""
-		tok.Type = token.EOF
-	}
-
-	l.readChar()
-	return tok
-}
-
-func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
-}
