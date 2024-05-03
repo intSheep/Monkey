007e70f11ebf4a8ebecbba6aa14e4c82d5bcfc65
intSheep <1098204193@qq.com>
2024-05-03 12:42:13 +0800

    test(object):add integer test

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,44 @@
+package evaluator
+
+import (
+	"Monkey/lexer"
+	"Monkey/object"
+	"Monkey/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			evaluated := testEval(tt.input)
+			testIntegerObject(t, evaluated, tt.expected)
+		})
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Fatalf("want got *object.Integet.but got [%v]", obj)
+	}
+	if result.Value != expected {
+		t.Fatalf("want get [%v], but got [%v]", expected, result.Value)
+		return false
+	}
+	return true
+}
